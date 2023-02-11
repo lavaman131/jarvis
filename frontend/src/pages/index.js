ae038ca42514025b50f0b257c985a23f56792a44
lavaman131 <lavaalex3@gmail.com>
2023-02-11 16:36:35 -0500

    changed readme img

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-import { headers } from "next.config";
+// import Image from "next/image";
+// import { Inter } from "@next/font/google";
+// import styles from "@/styles/Home.module.css";
+// import { headers } from "next.config";
 
 const API_URL = "http://localhost:8000/upload";
 
