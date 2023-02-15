a66cc0d50bd5a85e55ef70f3b8204b019fe1049c
lavaman131 <lavaalex3@gmail.com>
2023-02-14 22:41:21 -0500

    fixed frontend and backend integration

--- backend/api.py
+++ backend/api.py
@@ -4,7 +4,7 @@ from pydub import AudioSegment
 import whisper
 import openai
 import os
-openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
 
