a0260ed8183c4c1c4d95a51218b8ee6ca79bb9bf
lavaman131 <lavaalex3@gmail.com>
2023-04-09 09:36:28 -0400

    migrated to chat api

--- backend/app/api.py
+++ backend/app/api.py
@@ -6,15 +6,18 @@ import openai
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+
 load_dotenv(".env")
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.organization = os.environ.get("OPENAI_ORGANIZATION")
 
 app = FastAPI()
 
 origins = [
     "https://jarvis-kappa.vercel.app",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 app.add_middleware(
@@ -27,11 +30,12 @@ app.add_middleware(
 
 model = whisper.load_model("tiny")
 
+
 @app.post("/upload")
 async def upload(file: UploadFile):
     audio = AudioSegment.from_file(file.file, "ogg")
     audio.export("temp.ogg", format="ogg")
-    
+
     # load audio and pad/trim it to fit 30 seconds
     audio = whisper.load_audio("temp.ogg")
     audio = whisper.pad_or_trim(audio)
@@ -40,20 +44,31 @@ async def upload(file: UploadFile):
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
     # decode the audio
-    options = whisper.DecodingOptions(task="transcribe", language="en", fp16=False) # get rid of FP32 warning
+    options = whisper.DecodingOptions(
+        task="transcribe", language="en", fp16=False
+    )  # get rid of FP32 warning
     result = whisper.decode(model, mel, options)
     result = model.transcribe("temp.ogg")
     Path("temp.ogg").unlink(missing_ok=True)
-    
+
     try:
-        response = openai.Completion.create(
-                model="text-davinci-001",
-                prompt=result["text"],
-                temperature=0.6,
-                max_tokens=1024,
-                n=1)
-        
-        return {"answer": response["choices"][0]["text"].replace("\n\n", "")}
-    
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant.",
+                },
+                {"role": "user", "content": result["text"]},
+            ],
+            temperature=0.6,
+            max_tokens=1024,
+            n=1,
+        )
+
+        return {
+            "answer": response["choices"][0]["message"]["content"].replace("\n\n", "")
+        }
+
     except openai.error.OpenAIError as e:
         return {"answer": "Something went wrong. Try again later."}
