7901574fff67f51698fd6ebfccc121e450baf907
lavaman131 <lavaalex3@gmail.com>
2023-04-09 09:40:38 -0400

    migrated to chat api

--- backend/app/api.py
+++ backend/app/api.py
@@ -10,7 +10,6 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
-openai.organization = os.environ.get("OPENAI_ORGANIZATION")
 
 app = FastAPI()
 
